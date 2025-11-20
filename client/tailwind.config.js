0abf417cdf7cb1532e4d6c7ee3e0dc6a7c097267
Raghav <rgvndrak@gmail.com>
2025-11-20 08:49:08 +0530

    Initialisation

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      // START ADDING HERE
+      keyframes: {
+        shine: {
+          "0%": { "background-position": "100%" },
+          "100%": { "background-position": "-100%" },
+        },
+      },
+      animation: {
+        shine: "shine 5s linear infinite",
+      },
+      // END ADDING HERE
+    },
+  },
+  plugins: [],
+};
